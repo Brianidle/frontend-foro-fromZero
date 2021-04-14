bcc4fb685e521e91dea440424e51fe2c2bc8e3ab
SctBrian <ddbrayan10@hotmail.com>
2021-04-14 12:12:59 -0300

    refetchQueries option added so when the user create a new post, the GET_POSTS  cache querie updates with the data retrieved from a new request to the apollo server. this is neccesary because Apollo aggressively caches the queries

--- src/pages/newpost.js
+++ src/pages/newpost.js
@@ -2,11 +2,13 @@ import React, { useEffect } from "react";
 
 import { useMutation } from "@apollo/client";
 import { NEW_POST } from "../gql/mutation";
+import {GET_POSTS} from "../gql//query";
 
 import PostFromContainer from "../components/PostFormContainer";
 
 const NewPost = (props) => {
   const [newPost] = useMutation(NEW_POST, {
+    refetchQueries:[{query:GET_POSTS}],
     onCompleted: () => {
       props.history.push("/");
     },
