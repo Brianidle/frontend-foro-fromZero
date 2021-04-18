165e406d13263b40d295f2304b5874f353828f69
SctBrian <ddbrayan10@hotmail.com>
2021-04-18 10:38:20 -0300

    The condition for the method isAuthenticated() was replaced. New method 'logOut' which send a get request to the server, so that the server can replace the authentication cookies with an empty string

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { BACKEND_API_URI } from "../constantVariables";
+import { getJsonCookies } from "../helpers/cookieHelper";
 
 class Auth {
   isAuthenticated() {
-    if (localStorage.getItem("token")) {
+    let jsonCookies = getJsonCookies();
+
+    if (jsonCookies.user_session) {
       return true;
     } else {
       return false;
     }
+  }
+
+  logOut() {
+    fetch(BACKEND_API_URI + "/logout", {
+      method: "GET",
+      headers: {
+        "Content-Type": "text/plain;charset=UTF-8",
+      },
+      credentials: "include", // omit, include
+    });
   }
 }
 
