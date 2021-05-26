a447b41bf3f6e15ec5614b38443a77de70f0edc8
Brianidle <ddbrayan10@hotmail.com>
2021-05-26 12:20:44 -0300

    new dependencie installed, redux-thunk. Before rendering the app a request is executed to know if the client is authenticated or not.

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BACKEND_EXPRESS_API_URI } from './src/constantVariables';
 
 import App from './src/App';
 
-ReactDOM.render(<App/>,
-  document.getElementById('root')
-);
+const checkLoggedIn = async () => {
+  const response = await fetch(BACKEND_EXPRESS_API_URI + '/session', {
+    method: "GET",
+    credentials: 'include'
+  });
+  const { idUser } = await response.json();
+
+  let preloadedState = {};
+  if (idUser) {
+    preloadedState = {
+      tokenState: idUser
+    };
+  }
+  return preloadedState;
+};
+
+const renderApp = (preloadedState) => {
+  ReactDOM.render(<App preloadedState={preloadedState} />,
+    document.getElementById('root')
+  );
+}
+
+(async () => renderApp(await checkLoggedIn()))();
+
--- src/App.js
+++ src/App.js
@@ -3,72 +3,54 @@ import React from "react";
 import Pages from "./pages";
 import {
   ApolloClient,
-  ApolloLink,
   ApolloProvider,
   createHttpLink,
   InMemoryCache, from
 } from "@apollo/client";
 
-import { onError } from '@apollo/client/link/error';
-
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 
 import { changueTokenReducer } from "./reducers/changueToken";
-import { doChangueToken } from "./actions/actionCreators";
 
 import { BACKEND_GQL_API_URI } from './constantVariables';
 
+import { setContext } from "apollo-link-context";
+
+import thunk from "redux-thunk";
 
 const uri = BACKEND_GQL_API_URI;
 
 const httpLink = createHttpLink({ uri, credentials: "include" });
 const cache = new InMemoryCache();
 
-let rootReducer = combineReducers({
-  tokenState: changueTokenReducer,
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: "",
+    },
+  };
 });
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-const sessionAliveLink = new ApolloLink((operation, forward) => {
-  return forward(operation).map(response => {
-    const context = operation.getContext();
-
-    let responseFromServer = context.response;
-    //if the server rsponse with a status code of 230 that means the requests, previously sent, arrived with no cookies, or in other words it arrived with no autentication cookies
-    //so im not going to give autorization to the user.
-    if (responseFromServer.status != 230) {
-      if (responseFromServer.status == 200) {
-
-        if (!store.tokenState) {
-          store.dispatch(doChangueToken("Authorized"));
-        }
-      }
-    } else {
-      store.dispatch(doChangueToken(""));
-      console.log("Session Closed");
-    }
-
-    return response;
-  })
-})
-
-const logoutLink = onError(({ networkError }) => {
-  if (networkError.statusCode === 401) console.log("Session Closed");
-})
-
 const client = new ApolloClient({
-  link: from([logoutLink, sessionAliveLink, httpLink]),
+  link: authLink.concat(httpLink),
   cache,
   resolvers: {},
   connectToDevTools: true,
 });
 
-function App() {
+function App(preloadedState) {
+
+  let rootReducer = combineReducers({
+    tokenState: changueTokenReducer,
+  });
+
+  const store = createStore(
+    rootReducer,
+    preloadedState.preloadedState,
+    applyMiddleware(thunk)
+  );
 
   return (
     <ApolloProvider client={client}>
